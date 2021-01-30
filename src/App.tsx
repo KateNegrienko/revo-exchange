4c32d018b94259da36ef95394ee654c8877d4d80
Kate Negrienko <katya@negrienko.com>
2021-01-30 21:31:45 +0200

    add router, saga, redux, classnames, seamless-immutable

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ function App() {
   return (
     <div className={theme.App}>
       <header className={theme['App-header']}>
-        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-       
+        <a
+          className={theme['App-link']}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
