02d2691ab41421af69735feafdf69bbd0401ad99
Kate Negrienko <katya@negrienko.com>
2021-02-01 01:01:24 +0200

    move setNewPrice function to utils

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,4 +1,6 @@
 import { Account } from "../data/Account";
+import { Rate } from "../data/Rate";
+import { ExchangeAccountType } from "../pages/exchange/components/exchange-account/ExchangeAccount.interface";
 import { DataState } from "../reducers/account/account.types";
 
 export const exchangeMapping = (
@@ -21,4 +23,39 @@ export const exchangeMapping = (
     default:
       return { id, value };
   }
+};
+
+export const setNewPrice = (
+  rates: Rate[],
+  type: ExchangeAccountType,
+  price: number,
+  sourceAccount: Account,
+  destinationAccount: Account
+): { destinationPrice: string; sourcePrice: string } => {
+  const sourceRate = rates.find(({ id }: Rate) => id === sourceAccount.id)
+    ?.price;
+  const destinationRate = rates.find(
+    ({ id }: Rate) => id === destinationAccount.id
+  )?.price;
+  if (sourceRate && destinationRate) {
+    switch (type) {
+      case ExchangeAccountType.SOURCE:
+        const destination = (price * sourceRate) / destinationRate;
+        return {
+          destinationPrice: parseFloat(destination.toString()).toFixed(2),
+          sourcePrice: price.toString(),
+        };
+
+      case ExchangeAccountType.DESTINATION:
+        const source = (price * destinationRate) / sourceRate;
+        return {
+          destinationPrice: price.toString(),
+          sourcePrice: parseFloat(source.toString()).toFixed(2),
+        };
+    }
+  }
+  return {
+    destinationPrice: "",
+    sourcePrice: "",
+  };
 };
--- src/reducers/account/account.reducer.tsx
+++ src/reducers/account/account.reducer.tsx
@@ -1,6 +1,4 @@
-import { exchangeMapping } from "../../common/utils";
-import { Rate } from "../../data/Rate";
-import { ExchangeAccountType } from "../../pages/exchange/components/exchange-account/ExchangeAccount.interface";
+import { exchangeMapping, setNewPrice } from "../../common/utils";
 import * as constants from "./account.constants";
 import Model from "./account.model";
 import { DataState, Actions } from "./account.types";
@@ -20,30 +18,15 @@ export default function account(
       });
 
     case constants.SET_NEW_PRICE:
-      const sourceRate = payload.rates.find(
-        ({ id }: Rate) => id === state.sourceAccount.id
-      )?.price;
-      const destinationRate = payload.rates.find(
-        ({ id }: Rate) => id === state.destinationAccount.id
-      )?.price;
-      if (sourceRate && destinationRate) {
-        switch (payload.type) {
-          case ExchangeAccountType.SOURCE:
-            const destination = (payload.price * sourceRate) / destinationRate;
-            return state.merge({
-              destinationPrice: parseFloat(destination.toString()).toFixed(2),
-              sourcePrice: payload.price,
-            });
-
-          case ExchangeAccountType.DESTINATION:
-            const source = (payload.price * destinationRate) / sourceRate;
-            return state.merge({
-              destinationPrice: payload.price,
-              sourcePrice: parseFloat(source.toString()).toFixed(2),
-            });
-        }
-      }
-      return state;
+      return state.merge(
+        setNewPrice(
+          payload.rates,
+          payload.type,
+          payload.price,
+          state.sourceAccount,
+          state.destinationAccount
+        )
+      );
 
     case constants.EXCHANGE_MONEY:
       const accounts = state.accounts.map((item) =>
